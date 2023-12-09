5618b9aefc8788ec44f68c2bee771db3b6c019d0
Richard <richardto180@gmail.com>
2023-12-09 15:26:28 -0500

    Created variables for password inputs and create account button

---
+++ script.js
@@ -0,0 +1,5 @@
+const password = document.querySelector('#user-password').value;
+const confirmPassword = document.querySelector('#user-password-confirm').value;
+const createAccountBtn = document.querySelector('.create-account-btn');
+let passwordInput = '';
+
