9fdf4e5345653c7d02492f0daff072ed0426c01c
Richard <richardto180@gmail.com>
2023-12-09 15:36:08 -0500

    Added event listener to create account button

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const password = document.querySelector('#user-password').value;
-const confirmPassword = document.querySelector('#user-password-confirm').value;
+const password = document.querySelector('#user-password');
+const confirmPassword = document.querySelector('#user-password-confirm');
 const createAccountBtn = document.querySelector('.create-account-btn');
 let passwordInput = '';
+
+createAccountBtn.addEventListener('click', () => {
+  if (password.value == confirmPassword.value) {
+    console.log('Match') 
+  } else if (password.value !== confirmPassword.value) {
+    console.log('No Match')
+  }
+});
 
